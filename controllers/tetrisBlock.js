df5b7672c7f0bd31c5d77a846d271cea3bda274c
Jonathan Velkeneers <jonathanvelkeneers@hotmail.com>
2021-11-12 09:20:41 -0600

    Add PUT route

--- controllers/tetrisBlock.js
+++ controllers/tetrisBlock.js
@@ -42,8 +42,23 @@ exports.tetrisBlock_delete = function (req, res) {
     res.send('NOT IMPLEMENTED: TetrisBlock delete DELETE ' + req.params.id);
 };
 // Handle tetrisBlock update form on PUT.
-exports.tetrisBlock_update_put = function (req, res) {
-    res.send('NOT IMPLEMENTED: TetrisBlock update PUT' + req.params.id);
+exports.tetrisBlock_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await TetrisBlock.findById(req.params.id) // Do updates of properties
+        if (req.body.color)
+            toUpdate.color = req.body.color;
+        if (req.body.shape)
+            toUpdate.shape = req.body.shape;
+        if (req.body.numberOfSquares)
+            toUpdate.numberOfSquares = req.body.numberOfSquares;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`);
+    }
 };
 
 // VIEWS
